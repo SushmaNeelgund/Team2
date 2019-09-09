ee88b46d49e80768cfafe37b8bf094c6f461a065
User1 <user1@Skycliffitdomain.com>
2019-09-09 17:33:24 +0530

    updated1

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { NgbDropdownModule, NgbModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuNavComponent } from './menu-nav/menu-nav.component';
-import {   MatExpansionModule, MatToolbar, MatMenu, MatMenuTrigger, MatTabsModule, MatMenuModule, MatIconModule, MatSelectModule, MatToolbarModule, MatTab } from '@angular/material';
-import { HttpClientModule } from '@angular/common/http';
-import { MultiSelect } from '@syncfusion/ej2-dropdowns/src/multi-select';
+import { MatExpansionModule, MatTabsModule, MatMenuModule, MatIconModule,MatToolbarModule,MatCheckboxModule, MatAutocompleteModule } from '@angular/material';
+
 import { MultiSelectAllModule } from '@syncfusion/ej2-angular-dropdowns';
+import { AutoCompleteModule } from '@syncfusion/ej2-angular-dropdowns';
+import { DatePickerAllModule, DateTimePickerAllModule } from '@syncfusion/ej2-angular-calendars';
+import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
+import { ScheduleAllModule } from '@syncfusion/ej2-angular-schedule';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -21,13 +25,27 @@ import { MultiSelectAllModule } from '@syncfusion/ej2-angular-dropdowns';
         LayoutRoutingModule,
         TranslateModule,
         NgbDropdownModule,
-        MatExpansionModule,
-        HttpClientModule,
+
+        MatMenuModule,
+        MatIconModule,
+        MatTabsModule,
         MatToolbarModule,
-        MatTabsModule, MatMenuModule,MatIconModule,
-        MatSelectModule,NgbModule,
-        NgbPaginationModule,
-        MultiSelectAllModule
+        MatCheckboxModule,
+        MultiSelectAllModule,
+        AutoCompleteModule,
+        MatExpansionModule,
+        ScheduleAllModule,
+        DatePickerModule,
+        DatePickerAllModule,
+        DateTimePickerAllModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule,
+        FormsModule,
+        NgbModule
+
+
+
+
 
 
     ],
--- src/app/layout/menu-nav/menu-nav.component.ts
+++ src/app/layout/menu-nav/menu-nav.component.ts
@@ -2,20 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-// import { View, EventSettingsModel, GroupModel, ResourceDetails, TreeViewArgs, ActionEventArgs, EventFieldsMapping, PopupOpenEventArgs, RenderCellEventArgs } from '@syncfusion/ej2-schedule';
-import { extend } from 'webdriver-js-extender';
-// import { doctorData } from '../data';
-// import { ScheduleComponent } from '@syncfusion/ej2-angular-schedule';
-// import { addClass } from '@syncfusion/ej2-base';
-// import { FormControl } from '@angular/forms';
-import {startWith, map} from 'rxjs/operators';
-
-import { DataService } from 'src/app/layout/menu-nav/data.service';
 import { Specaility } from './speciality';
-// import { Specaility, Practice } from '../speciality';
-// import { MenuNavbarService } from '../menu-navbar.service';
-
+import { DataService } from './data.service';
+import { doctorData } from './data';
+import { extend } from '@syncfusion/ej2-base/src/util';
+import { addClass } from '@syncfusion/ej2-base';
+import { ScheduleComponent, TreeViewArgs, ResourceDetails, ActionEventArgs, EventFieldsMapping, PopupOpenEventArgs, RenderCellEventArgs, GroupModel, EventSettingsModel, View } from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-menu-nav',
@@ -29,7 +21,7 @@ export class MenuNavComponent implements OnInit {
    closeResult: string;
     selectedOption:string;
     control = new FormControl();
-     arr4:Specaility[]=[];
+     public arr:Specaility[]=[];
 
      public localFields: Object = { text: "Name"};
      public localWaterMark: string = 'Select practice';
@@ -88,123 +80,129 @@ export class MenuNavComponent implements OnInit {
           //enable the highlight property to highlight the matched character in suggestion list
           public autofill2: Boolean = true;
 
+           public arr4:Specaility[]=[];
+            // maps the appropriate column to fields property
+            public fields3: Object = { value: "speciality" };
+            // set the placeholder to the AutoComplete input
+            public text3: string = "Find a speciality";
+            //enable the highlight property to highlight the matched character in suggestion list
+            public autofill3: Boolean = true;
 
 
 
 
 
 
-//       public data: Object[] = <Object[]>extend([], doctorData);
-//       public selectedDate: Date = new Date(2018, 3, 1);
-//       public currentView: View = 'WorkWeek';
-//       public allowResizing: boolean = false;
-//       public allowDragDrop: boolean = false;
-//       public resourceDataSource: Object[] = [
-
-//           { text: 'Hema@ 38 Astoria', id: 1, color: '',  workDays: [1, 2],startHour: '02:00', endHour: '07:00' },
-//           { text: 'Hema@ 45 DEAN', id: 2, color: '', workDays: [1, 3, 5], startHour: '12:00', endHour: '08:00' },
-//           { text: 'Hema@ 45 West NY ', id: 3, color: '', startHour: '11:00', endHour: ':00' }
-//       ];
-//       public group: GroupModel = { resources: ['Doctors'] };
-//       public eventSettings: EventSettingsModel = {
-//           dataSource: this.data,
-//           fields: {
-//               subject: { title: 'Service Type', name: 'Subject' },
-//               location: { title: 'Patient Name', name: 'Location' },
-//               description: { title: 'Summary', name: 'Description' },
-//               startTime: { title: 'From', name: 'StartTime' },
-//               endTime: { title: 'To', name: 'EndTime' }
-//           }
-//       };
-//       // @ViewChild('scheduleObj')
-//       public scheduleObj: ScheduleComponent;
+//
   constructor(private modalService: NgbModal,private _data:DataService) { }
-//     getDoctorName(value: ResourceDetails | TreeViewArgs): string {
-//       return ((value as ResourceDetails).resourceData) ?
-//           (value as ResourceDetails).resourceData[(value as ResourceDetails).resource.textField] as string
-//           : (value as TreeViewArgs).resourceName;
-//   }
-//   getDoctorImage(value: ResourceDetails | TreeViewArgs): string {
-//       let resourceName: string = this.getDoctorName(value);
-//       return resourceName.replace(' ', '-').toLowerCase();
-//   }
-//   getDoctorLevel(value: ResourceDetails | TreeViewArgs): string {
-//       let resourceName: string = this.getDoctorName(value);
-//       return (resourceName === 'Will Smith') ? 'Capiola david' :  ( resourceName === '') ? '' : 'Dr Capiola David';
-//   }
 
-//   onActionBegin(args: ActionEventArgs): void {
-//       let isEventChange: boolean = (args.requestType === 'eventChange');
-//       if ((args.requestType === 'eventCreate' && (<Object[]>args.data).length > 0) || isEventChange) {
-//           let eventData: { [key: string]: Object } = (isEventChange) ? args.data as { [key: string]: Object } :
-//               args.data[0] as { [key: string]: Object };
-//           let eventField: EventFieldsMapping = this.scheduleObj.eventFields;
-//           let startDate: Date = eventData[eventField.startTime] as Date;
-//           let endDate: Date = eventData[eventField.endTime] as Date;
-//           let resourceIndex: number = [1, 2, 3].indexOf(eventData.DoctorId as number);
-//           args.cancel = !this.isValidateTime(startDate, endDate, resourceIndex);
-//           if (!args.cancel) {
-//               args.cancel = !this.scheduleObj.isSlotAvailable(startDate, endDate, resourceIndex);
-//           }
-//       }
-//   }
+  public data: Object[] = <Object[]>extend([], doctorData);
+  public selectedDate: Date = new Date(2018, 3, 1);
+  public currentView: View = 'WorkWeek';
+  public allowResizing: boolean = false;
+  public allowDragDrop: boolean = false;
+  public resourceDataSource: Object[] = [
 
-//   isValidateTime(startDate: Date, endDate: Date, resIndex: number): boolean {
-//       let resource: ResourceDetails = this.scheduleObj.getResourcesByIndex(resIndex);
-//       let startHour: number = parseInt(resource.resourceData.startHour.toString().slice(0, 2), 10);
-//       let endHour: number = parseInt(resource.resourceData.endHour.toString().slice(0, 2), 10);
-//       return (startHour <= startDate.getHours() && endHour >= endDate.getHours());
-//   }
+      { text: 'Hema@ 38 Astoria', id: 1, color: '',  workDays: [1, 2],startHour: '02:00', endHour: '07:00' },
+      { text: 'Hema@ 45 DEAN', id: 2, color: '', workDays: [1, 3, 5], startHour: '12:00', endHour: '08:00' },
+      { text: 'Hema@ 45 West NY ', id: 3, color: '', startHour: '11:00', endHour: ':00' }
+  ];
+  public group: GroupModel = { resources: ['Doctors'] };
+  public eventSettings: EventSettingsModel = {
+      dataSource: this.data,
+      fields: {
+          subject: { title: 'Service Type', name: 'Subject' },
+          location: { title: 'Patient Name', name: 'Location' },
+          description: { title: 'Summary', name: 'Description' },
+          startTime: { title: 'From', name: 'StartTime' },
+          endTime: { title: 'To', name: 'EndTime' }
+      }
+  };
+  // @ViewChild('scheduleObj')
+  public scheduleObj: ScheduleComponent;
 
-//   onPopupOpen(args: PopupOpenEventArgs): void {
-//       if (args.target && args.target.classList.contains('e-work-cells')) {
-//           args.cancel = !args.target.classList.contains('e-work-hours');
-//       }
-//   }
+getDoctorName(value: ResourceDetails | TreeViewArgs): string {
+  return ((value as ResourceDetails).resourceData) ?
+      (value as ResourceDetails).resourceData[(value as ResourceDetails).resource.textField] as string
+      : (value as TreeViewArgs).resourceName;
+}
+getDoctorImage(value: ResourceDetails | TreeViewArgs): string {
+  let resourceName: string = this.getDoctorName(value);
+  return resourceName.replace(' ', '-').toLowerCase();
+}
+getDoctorLevel(value: ResourceDetails | TreeViewArgs): string {
+  let resourceName: string = this.getDoctorName(value);
+  return (resourceName === 'Will Smith') ? 'Capiola david' :  ( resourceName === '') ? '' : 'Dr Capiola David';
+}
 
-//   onRenderCell(args: RenderCellEventArgs): void {
-//       if (args.element.classList.contains('e-work-hours') || args.element.classList.contains('e-work-cells')) {
-//           addClass([args.element], ['willsmith', 'alice', 'robson'][parseInt(args.element.getAttribute('data-group-index'), 10)]);
-//       }
-//   }
+onActionBegin(args: ActionEventArgs): void {
+  let isEventChange: boolean = (args.requestType === 'eventChange');
+  if ((args.requestType === 'eventCreate' && (<Object[]>args.data).length > 0) || isEventChange) {
+      let eventData: { [key: string]: Object } = (isEventChange) ? args.data as { [key: string]: Object } :
+          args.data[0] as { [key: string]: Object };
+      let eventField: EventFieldsMapping = this.scheduleObj.eventFields;
+      let startDate: Date = eventData[eventField.startTime] as Date;
+      let endDate: Date = eventData[eventField.endTime] as Date;
+      let resourceIndex: number = [1, 2, 3].indexOf(eventData.DoctorId as number);
+      args.cancel = !this.isValidateTime(startDate, endDate, resourceIndex);
+      if (!args.cancel) {
+          args.cancel = !this.scheduleObj.isSlotAvailable(startDate, endDate, resourceIndex);
+      }
+  }
+}
+
+isValidateTime(startDate: Date, endDate: Date, resIndex: number): boolean {
+  let resource: ResourceDetails = this.scheduleObj.getResourcesByIndex(resIndex);
+  let startHour: number = parseInt(resource.resourceData.startHour.toString().slice(0, 2), 10);
+  let endHour: number = parseInt(resource.resourceData.endHour.toString().slice(0, 2), 10);
+  return (startHour <= startDate.getHours() && endHour >= endDate.getHours());
+}
+
+onPopupOpen(args: PopupOpenEventArgs): void {
+  if (args.target && args.target.classList.contains('e-work-cells')) {
+      args.cancel = !args.target.classList.contains('e-work-hours');
+  }
+}
+
+onRenderCell(args: RenderCellEventArgs): void {
+  if (args.element.classList.contains('e-work-hours') || args.element.classList.contains('e-work-cells')) {
+      addClass([args.element], ['willsmith', 'alice', 'robson'][parseInt(args.element.getAttribute('data-group-index'), 10)]);
+  }
+}
+
 
     ngOnInit() {
-    //   this.filteredStreets = this.control.valueChanges.pipe(
-    //     startWith(''),
-    // map(value => this._filter(value))
-    //   );
+        this._data.getAllSpeciality() .subscribe(
+            (data:Specaility[]) =>{
+      this.arr4 = data;
 
-    this._data.getAllSpeciality().subscribe(
-      (data:Specaility[]) => {
-        this.arr4 = data;
-     },
-    );
+            }
+          );
 
-
-    // this._data.getAllLocation().subscribe(
-    //   (data:Location[]) => {
-    //     this.arr1 = data;
-
-    //   },
-    // );
     }
-    openEdit(contents) {
-      this.modalService.open(contents, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-        this.closeResult = `Closed with: ${result}`;
-      }, (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      });
-      this.modalService.open(contents, {size:'lg'});
-    }
-    private getDismissReason(reason: any): string {
-      if (reason === ModalDismissReasons.ESC) {
-        return 'by pressing ESC';
-      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-        return 'by clicking on a backdrop';
-      } else {
-        return  `with: ${reason}`;
+
+
+
+
+    openEdit(forms) {
+
+        // console.log('popup open');
+        // this.modalService.open(forms, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+        //   this.closeResult = `Closed with: ${result}`;
+        // }, (reason) => {
+        //   this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        // });
+         this.modalService.open(forms, {size:'lg'});
       }
-    }
+      private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+          return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+          return 'by clicking on a backdrop';
+        } else {
+          return  `with: ${reason}`;
+        }
+      }
 
 
 
